eb93c8e4d0030cba936692f900b51205103a6267
Thomas Black <sheepoox@gmail.com>
2018-05-01 18:44:09 +0200

    Fixture docstrings.

--- tests/actors/fixtures.py
+++ tests/actors/fixtures.py
@@ -1,18 +1,27 @@
 import pytest
 
-from fourier.actors import Circle, SineWave, LineAxes
+from fourier.actors import Circle, LineAxes, SineWave
 
 
 @pytest.fixture
 def circle():
+    """
+    Instance of Circle.
+    """
     return Circle(radius=2, x0=1, y0=-1)
 
 
 @pytest.fixture
 def sine_wave():
+    """
+    Instance of SineWave.
+    """
     return SineWave(2)
 
 
 @pytest.fixture
 def line_axes():
+    """
+    Instance of LineAxes.
+    """
     return LineAxes()
